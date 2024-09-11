57e6c20e7ca8d2b816f91836ee3677f9969d08d7
collinsmecosy <collinsmecosy@gmail.com>
2024-09-11 16:29:56 +0000

    another attempt to configure styles

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    // Enable App Router
+    experimental: {
+      appDir: true,
+    },
+  }
+  
+  export default nextConfig
