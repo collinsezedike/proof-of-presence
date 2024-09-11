1b0c5d71ddfe4379b8a87af44570c454dac34b7b
collinsmecosy <collinsmecosy@gmail.com>
2024-09-11 13:12:03 +0000

    added some packages to handle the implementation of the css styling in across the page

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,9 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
-};
+  /** @type {import('postcss-load-config').Config} */
+  const config = {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  };
 
-export default config;
+  export default config;
