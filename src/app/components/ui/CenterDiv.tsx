7b78714347292af2ada3acb5fb7ed6cb89fcbfd7
Ezedike Evan <120946193+Ezedike-Evan@users.noreply.github.com>
2024-09-12 10:34:12 +0100

    Update CenterDiv.tsx

--- src/app/components/ui/CenterDiv.tsx
+++ src/app/components/ui/CenterDiv.tsx
@@ -1,1 +1,16 @@
+import React from "react";
 
+const CenterDiv = ({children})=>{
+    return <div
+        style={{
+            border:'1px solid transparent',
+            borderRadius:'10px',
+            display: 'flex',
+          flexDirection: 'column'
+        }}
+    >
+      { children }
+    </div>
+}
+
+export default CenterDiv
