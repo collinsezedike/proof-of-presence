6e54297dc8e92b02cc479230456a6bb46108cab9
CollinsEzeokeke <collinsmecosy@gmail.com>
2024-09-13 00:31:29 +0000

    made some configurations

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    // Enable App Router
-    experimental: {
-      appDir: true,
-    },
-  }
-  
-  export default nextConfig
+const nextConfig = {};
+
+export default nextConfig;
